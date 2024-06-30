17ee5c841aef6987efa0eac246209bea3561c394
NANDbrew <zortman@gmail.com>
2024-06-30 13:28:06 -0500

    update version

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ namespace ShipyardExpansion
     {
         public const string PLUGIN_ID = "com.nandbrew.shipyardexpansion";
         public const string PLUGIN_NAME = "Shipyard Expansion";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "0.2.0";
 
         internal const int mastListSize = 64;
 
