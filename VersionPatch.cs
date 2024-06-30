82b10f15e3a33721271b1594bb489cd4ad2930f5
NANDbrew <zortman@gmail.com>
2024-06-30 15:37:43 -0500

    add mod version save tracking

---
+++ VersionPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace ShipyardExpansion
+{
+    [HarmonyPatch(typeof(SaveLoadManager), "SaveModData")]
+    internal static class VersionPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (!GameState.modData.ContainsKey(Plugin.PLUGIN_ID))
+            {
+                GameState.modData.Add(Plugin.PLUGIN_ID, Plugin.PLUGIN_VERSION);
+            }
+            else
+            {
+                GameState.modData[Plugin.PLUGIN_ID] = Plugin.PLUGIN_VERSION;
+            }
+        }
+    }
+}
