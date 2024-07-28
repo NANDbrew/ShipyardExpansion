fd7ab2ffa724592eb7ed45ac6fdcd565e58dc614
NANDbrew <zortman@gmail.com>
2024-07-28 11:00:19 -0500

    kakam: remove second middlestay

--- ShipPatches/KakamPatches.cs
+++ ShipPatches/KakamPatches.cs
@@ -20,6 +20,8 @@ namespace ShipyardExpansion
             Mast mizzenMastM = mizzenMast.GetComponent<Mast>();
             //Transform shrouds = container.Find("Cylinder_002");
             //var shroudAnchor = container.Find("static_rig_001");
+            Transform forestay = container.Find("forestay");
+            Mast forestayM = forestay.GetComponent<Mast>();
             Transform lowerForestay = container.Find("forestay_002");
             Mast lowerForestayM = lowerForestay.GetComponent<Mast>();
             var angleWinches = new GPButtonRopeWinch[2] { mizzenMastM.leftAngleWinch[0], mizzenMastM.rightAngleWinch[0] };
@@ -37,6 +39,9 @@ namespace ShipyardExpansion
             mizzenMastM.leftAngleWinch = new GPButtonRopeWinch[1] { mizzenMastM.leftAngleWinch[1] };
             mizzenMastM.rightAngleWinch = new GPButtonRopeWinch[1] { mizzenMastM.rightAngleWinch[1] };
             mizzenMastM.GetComponent<Mast>().mastHeight += 0.75f;
+            var m1brace = mizzenMast.Find("mast_holder_001");
+            m1brace.localPosition = new Vector3(-1.29f, 0, -10.12f);
+            m1brace.localScale = new Vector3(0.87f, 0.87f, 0.94f);
             //mizzenMastM.GetComponent<Mast>().startSailHeightOffset -= 0.5f;
             var ropeHolder_003 = mainMast1.Find("rope_holder_003");
             mainMast1.GetComponent<Mast>().midRopeAtt[0].parent = ropeHolder_003;
@@ -53,7 +58,7 @@ namespace ShipyardExpansion
             var midstayReefs = Util.CopyWinches(mizzenMast.GetComponent<Mast>().reefWinch, Vector3.zero, Vector3.zero);
             var angleWinches2 = Util.CopyWinches(angleWinches, Vector3.zero, new Vector3(0.3f, -0.05f, 0));
 
-            Mast midstay_lower = Util.CopyMast(lowerForestay, new Vector3(-2f, 7f, 0f), lowerForestay.localEulerAngles, new Vector3(1f, 1f, 1.37f), "midstay_lower", "lower midstay", 31);
+            /*Mast midstay_lower = Util.CopyMast(lowerForestay, new Vector3(-2f, 7f, 0f), lowerForestay.localEulerAngles, new Vector3(1f, 1f, 1.37f), "midstay_lower", "lower midstay", 31);
             midstay_lower.mastReefAtt = mizzenMastM.mastReefAtt;
             midstay_lower.reefWinch = new GPButtonRopeWinch[1] { midstayReefs[0] };
             midstay_lower.reefWinch[0].transform.localPosition = new Vector3(-2.87f, midstayReefs[0].transform.localPosition.y, 0f);
@@ -64,17 +69,18 @@ namespace ShipyardExpansion
             midstay_lower.mastHeight = 7.5f;
             midstay_lower.GetComponent<BoatPartOption>().requires = new List<BoatPartOption> { mainMast1.GetComponent<BoatPartOption>(), mizzenMast.GetComponent<BoatPartOption>() };
             partsList.availableParts[7].partOptions.Add(midstay_lower.GetComponent<BoatPartOption>());
-
-            Mast midstay_upper = Util.CopyMast(lowerForestay, new Vector3(-2f, 10.5f, 0f), lowerForestay.localEulerAngles, new Vector3(1f, 1f, 1.37f), "midstay_upper", "top midstay", 32);
+*/
+            //Mast midstay_upper = Util.CopyMast(lowerForestay, new Vector3(-2f, 10.5f, 0f), lowerForestay.localEulerAngles, new Vector3(1f, 1f, 1.654f), "midstay_upper", "top midstay", 32);
+            Mast midstay_upper = Util.CopyMast(forestay, new Vector3(-2.4f, 10.6f, 0f), forestay.localEulerAngles, new Vector3(1, 1, 1.14f), "midstay", "middlestay", 32);
             midstay_upper.mastReefAtt = mizzenMastM.mastReefAtt;
             midstay_upper.reefWinch = new GPButtonRopeWinch[1] { midstayReefs[1] };
             midstay_upper.reefWinch[0].transform.localPosition = new Vector3(-3.27f, midstayReefs[1].transform.localPosition.y, 0f);
             midstay_upper.reefWinch[0].transform.localEulerAngles = new Vector3(0, 270, 90);
             midstay_upper.reefWinch[0].rope = null;
-            midstay_upper.leftAngleWinch = new GPButtonRopeWinch[1] { angleWinches2[0] };
-            midstay_upper.rightAngleWinch = new GPButtonRopeWinch[1] { angleWinches2[1] };
-            midstay_upper.mastHeight = 7.5f;
-            midstay_upper.GetComponent<BoatPartOption>().requires = midstay_lower.GetComponent<BoatPartOption>().requires;
+            midstay_upper.leftAngleWinch = new GPButtonRopeWinch[1] { angleWinches[0] };
+            midstay_upper.rightAngleWinch = new GPButtonRopeWinch[1] { angleWinches[1] };
+            midstay_upper.mastHeight = 10f;
+            midstay_upper.GetComponent<BoatPartOption>().requires = new List<BoatPartOption> { mainMast1.GetComponent<BoatPartOption>(), mizzenMast.GetComponent<BoatPartOption>() };
             var midstay_upper_none = Util.CreatePartOption(container, "(no top midstay)", "(no top midstay)");
             Util.CreateAndAddPart(partsList, 2, new List<BoatPartOption> { midstay_upper_none, midstay_upper.GetComponent<BoatPartOption>() });
 
