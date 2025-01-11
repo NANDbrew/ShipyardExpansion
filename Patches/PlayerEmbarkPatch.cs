06542a03b2c29ed89b829901410f8c2fb8306ce4
NANDbrew <zortman@gmail.com>
2025-01-11 15:52:14 -0600

    added patch to prevent disembarking during ladder animation

---
+++ Patches/PlayerEmbarkPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using ShipyardExpansion.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipyardExpansion.Patches
+{
+    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "Update")]
+    internal static class PlayerEmbarkPatch
+    {
+        public static bool Prefix(bool __runOriginal)
+        {
+            if (NANDLadder.animating || !__runOriginal) return false;
+            return true;
+
+        }
+
+    }
+}
