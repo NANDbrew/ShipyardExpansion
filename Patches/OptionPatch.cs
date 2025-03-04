ce53726fd84a8804ccf136d828c93a46e95cfa55
NANDbrew <zortman@gmail.com>
2025-03-04 14:03:41 -0600

    bump version number

---
+++ Patches/OptionPatch.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ShipyardExpansion.Patches
+{
+    [HarmonyPatch(typeof(BoatPart), "SetOptionEnabled")]
+    internal static class OptionPatch
+    {
+        public static void Postfix(int i, bool state, List<BoatPartOption> ___partOptions)
+        {
+            //___partOptions[i].gameObject.SetActive(state);
+            GameObject[] childOptions = ___partOptions[i].childOptions;
+            for (int j = 0; j < childOptions.Length; j++)
+            {
+                childOptions[j].SetActive(state);
+                if (childOptions[j].GetComponent<Mast>() is Mast mast)
+                {
+                    mast.Awake();
+                }
+
+            }
+        }
+    }
+}
+*/
--- Plugin.cs
+++ Plugin.cs
@@ -15,17 +15,15 @@ namespace ShipyardExpansion
     {
         public const string PLUGIN_ID = "com.nandbrew.shipyardexpansion";
         public const string PLUGIN_NAME = "Shipyard Expansion";
-        public const string PLUGIN_VERSION = "0.5.10";
+        public const string PLUGIN_VERSION = "0.5.11";
 
         internal const int mastListSize = 128;
         internal const int sailListSize = 512;
 
+        internal static Plugin instance;
         public static List<BoatCustomParts> moddedBoats;
-        //public static List<BoatPartOption> stockPartOptions;
         public static Dictionary<BoatPart, int> stockParts;
-        //public static List<Mast> stockMasts;
         public static int stockSailsListSize;
-        //public static Dictionary<int, SaveBoatCustomizationData> stockConfigs;
         public static Dictionary<GameObject, SaveBoatCustomizationData> converted;
         public static Transform prefabContainer;
 
@@ -36,24 +34,19 @@ namespace ShipyardExpansion
         internal static ConfigEntry<bool> lenientSquares;
         internal static ConfigEntry<bool> vertLateens;
         internal static ConfigEntry<bool> vertFins;
-
         internal static ConfigEntry<bool> unrollSails;
-
         internal static ConfigEntry<bool> addSails;
         internal static ConfigEntry<bool> cleanLoad;
         internal static ConfigEntry<bool> percentSailNames;
         internal static ConfigEntry<bool> autoFit;
         internal static ConfigEntry<bool> skipSailData;
         internal static ConfigEntry<bool> starterSetFix;
-
         internal static ConfigEntry<int> climbSpeed;
-        internal static Plugin instance;
+
         private void Awake()
         {
             instance = this;
-            //stockPartOptions = new List<BoatPartOption>();
             stockParts = new Dictionary<BoatPart, int>();
-            //stockMasts = new List<Mast>();
             moddedBoats = new List<BoatCustomParts>();
             converted = new Dictionary<GameObject, SaveBoatCustomizationData>();
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
@@ -63,7 +56,6 @@ namespace ShipyardExpansion
             cleanLoad = Config.Bind("Fixers", "Clean load", true, new ConfigDescription("Enable this before loading a broken save"));
             skipSailData = Config.Bind("Fixers", "skip sail data", false, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             starterSetFix = Config.Bind("Fixers", "starter set fix", true, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
-
             vertLateens = Config.Bind("Slant", "Vertical lateens", true, new ConfigDescription("Install lateens vertical instead of slanting with the mast"));
             vertFins = Config.Bind("Slant", "Vertical fins", true, new ConfigDescription("Install fin sails vertical instead of slanting with the mast"));
             unrollSails = Config.Bind("Settings", "Unfurl sails in shipyard", true, new ConfigDescription("Unfurl existing sails when entering the shipyard"));
