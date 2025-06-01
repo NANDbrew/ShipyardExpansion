e7236ffed7faedbaf81b755e0d2bcbe920357d01
NANDbrew <zortman@gmail.com>
2025-06-01 04:44:45 -0500

    Add patch to SailConnections, fixing mis-ordered rope sections

---
+++ Patches/SailConnectionPatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ShipyardExpansion.Patches
+{
+    [HarmonyPatch(typeof(SailConnections), "Awake")]
+    internal static class SailConnectionPatch
+    {
+        internal static void Postfix(SailConnections __instance)
+        {
+            if (__instance.mastReefAttachment.GetComponent<RopeEffect>().attachment == __instance.mastReefAttExtension)
+            {
+                // magical reference swapping
+                (__instance.mastReefAttExtension, __instance.mastReefAttachment) = (__instance.mastReefAttachment, __instance.mastReefAttExtension);
+
+                // what it's doing:
+                //var att = __instance.mastReefAttachment;
+                //__instance.mastReefAttachment = __instance.mastReefAttExtension;
+                //__instance.mastReefAttExtension = att;
+            }
+        }
+    }
+}
