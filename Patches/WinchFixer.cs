30122ce7eb50c4f99cd377ef24bef0e8cb08373f
NANDbrew <zortman@gmail.com>
2026-08-15 23:56:08 -0500

    imported winch fixer from testbed. added an editor winch fixer

---
+++ Patches/WinchFixer.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ShipyardExpansion.Patches
+{
+    [HarmonyPatch(typeof(GPButtonRopeWinch), "Awake")]
+    internal static class WinchFixer
+    {
+        public static void Postfix(GPButtonRopeWinch __instance)
+        {
+            if (__instance.transform.localEulerAngles.x % 90 == 0)
+            {
+                __instance.transform.Rotate(0.1f, 0f, 0f, Space.Self);
+                Debug.Log("SE: rotated " + __instance.name);
+            }
+        }
+    }
+}
